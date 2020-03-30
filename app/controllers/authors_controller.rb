72d21006ac38c530332684e88bbc69663e07d179
Teerth Patel <teerth1205@gmail.com>
2020-03-30 20:08:00 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,16 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    
+    @author = Author.new(author_params)
+    
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      @author.errors.full_messages
+      render :new
+    end 
   end
 
   private
